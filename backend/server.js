c137ce77388bc14a4a3b0e1c0bd61e4adbba18a6
HasiniIshara <hasiniisharao4@gmail.com>
2024-05-29 19:43:17 +0300

    updated files

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+// const passport = require("passport");
+// const authRoutes = require('./routes/authRoute')
+// const passportConfig = require('./middleware/passportConfig')
+// const { swaggerUi, swaggerDocs } = require('./swagger');
+
+
+const app = express();
+
+passportConfig(passport);
+
+// // middle ware
+// app.use(express.json());
+// app.use(passport.initialize())
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// app.use((req, res, next)=>{
+//   console.log(req.path, req.method)
+//   next()
+// })
+
+
+// // routes
+// app.use('/api/auth', authRoutes)
+
+// Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("app running on PORT 3000");
+    });
+  })
+  .catch((error) => {
+    console.log("the error", error);
+  });
