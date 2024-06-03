bd9f7d103ec38569ce0b58b25b16e3ce67d31381
HasiniIshara <hasiniisharao4@gmail.com>
2024-06-03 23:04:13 +0300

    create frontend navbar

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Box, Container, Typography, Link } from '@mui/material';
+
+export default function Login(): React.ReactElement {
+  return (
+    <>
+     <p>Login page</p>
+     <p>Login page</p>
+     <p>Login page</p>
+     <p>Login page</p>
+     <p>Login page</p>
+     <p>Login page</p>
+     <p>Login page</p>
+     </>
+     
+     
+
+  );
+}
---
+++ frontend/src/components/Register.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Box, Container, Typography, Link } from '@mui/material';
+
+export default function Register(): React.ReactElement {
+  return (
+    <>
+     <p>Register page</p>
+     <p>Register page</p>
+     <p>Register page</p>
+     <p>Register page</p>
+     <p>Register page</p>
+     </>
+
+  );
+}
