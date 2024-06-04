0d56a75ebdc1a5b3eaef89cbb662e016d9cec5fd
HasiniIshara <hasiniisharao4@gmail.com>
2024-06-04 17:25:54 +0300

    create frontend login and register components.

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,19 +1,82 @@
-import React from 'react';
-import { Box, Container, Typography, Link } from '@mui/material';
+import { LockOutlined } from "@mui/icons-material";
+import {
+  Container,
+  CssBaseline,
+  Box,
+  Avatar,
+  Typography,
+  TextField,
+  Button,
+  Grid,
+} from "@mui/material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function Login(): React.ReactElement {
+export default function Login() {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {};
+
   return (
     <>
-     <p>Login page</p>
-     <p>Login page</p>
-     <p>Login page</p>
-     <p>Login page</p>
-     <p>Login page</p>
-     <p>Login page</p>
-     <p>Login page</p>
-     </>
-     
-     
+      <Container maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            mt: 20,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "primary.light" }}>
+            <LockOutlined />
+          </Avatar>
+          <Typography variant="h5">Sign In</Typography>
+          <Box sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="User Name"
+              name="username"
+              autoFocus
+              value={username}
+              onChange={(e) => setUserName(e.target.value)}
+            />
 
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+
+            <Button
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleLogin}
+            >
+              Sign In
+            </Button>
+            <Grid container justifyContent={"flex-end"}>
+              <Grid item>
+                <Link to="/signup">Don't have an account? Register</Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </>
   );
-}
+};
