cb0f61a647f9df4c05c48c463f513702c2391dcb
HasiniIshara <hasiniisharao4@gmail.com>
2024-06-26 20:13:12 +0300

    Create slot bookings

--- backend/routes/parkingRoutes.ts
+++ backend/routes/parkingRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createParkingSlot,getAllParkingSlots } from '../controller/parkingController';
+import { createParkingSlot,getAllParkingSlots, bookParkingSlot } from '../controller/parkingController';
 import { checkRoles }  from '../middleware/checkRole';
 const passport = require('passport');
 
@@ -8,5 +8,7 @@ const router = Router();
 router.post('/createParkingSlot', passport.authenticate("jwt", { session: false }), checkRoles(["Admin"]) as any, createParkingSlot);
 
 router.get("/viewAll", passport.authenticate("jwt", { session: false }), getAllParkingSlots);
+
+router.patch("/:id", passport.authenticate("jwt", { session: false }), bookParkingSlot);
 
 export default router;
